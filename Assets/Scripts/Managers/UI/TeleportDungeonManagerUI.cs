76327cda6ea774c163043a1d1ef8df71b1a1affc
ImSkrub <estebanaberastury@gmail.com>
2025-10-11 09:52:48 -0300

    Revert "Avances Puerta y Clientes"
    This reverts commit e34c6429a40f229b1aff206297af1a4a09881a80.

--- Assets/Scripts/Managers/UI/TeleportDungeonManagerUI.cs
+++ Assets/Scripts/Managers/UI/TeleportDungeonManagerUI.cs
@@ -6,7 +6,6 @@ using UnityEngine.EventSystems;
 public class TeleportDungeonManagerUI : MonoBehaviour
 {
     private PlayerModel playerModel;
-    private TeleportDungeonUI teleportDungeonUI;
 
     [SerializeField] private GameObject panelTeleport;
     [SerializeField] private List<GameObject> buttonsTeleportPanel;
@@ -75,10 +74,8 @@ public class TeleportDungeonManagerUI : MonoBehaviour
     // Funcion asignada a boton en la UI
     public void ButtonNo()
     {
-        StartCoroutine(teleportDungeonUI.MoveDoorsCoroutine(DoorAnimationType.Close));
         onClearSelectedCurrentGameObject?.Invoke();
         ignoreFirstButtonSelected = true;
-        teleportDungeonUI.IsOpenByPlayer = false;
         playerModel.IsInTeleportPanel = false;
         DeviceManager.Instance.IsUIModeActive = false;
         panelTeleport.SetActive(false);
@@ -135,7 +132,6 @@ public class TeleportDungeonManagerUI : MonoBehaviour
     private void GetComponents()
     {
         playerModel = FindFirstObjectByType<PlayerModel>();
-        teleportDungeonUI = FindFirstObjectByType<TeleportDungeonUI>();
     }
 
     private void CheckLastSelectedButtonIfAdminPanelIsOpen()
